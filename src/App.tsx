a6f273a7648a232e5b01be2c3a03e360d71893d6
Sreshta K <sreshtakumari680@gmail.com>
2023-09-19 01:37:51 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import quotes from "./assets/quotes.json";
+import {FaTwitter,FaQuoteLeft,FaQuoteRight} from "react-icons/fa";
+import "./App.css";
+interface Quote{
+  quote: string;
+  author: string;
+}
+const getRandomQuote=(): Quote => {
+  return quotes[Math.floor(Math.random()*quotes.length)];
+};
+const getRandomColor=(): string => {
+  const red = Math.floor(Math.random()*128);
+  const green = Math.floor(Math.random()*128);
+  const blue= Math.floor(Math.random()*128);
+  return `rgb(${red},${green},${blue})`;
+};
+const transition= "all 1s";
+
+function App() {
+  const [quote, setQuote] = useState<Quote>(getRandomQuote());
+  const[randomColor,setRandomColr]=useState<string>(getRandomColor());
+   
+    
+  const changeQuote= () => {
+    setQuote(getRandomQuote());
+  };
+  return (
+    <div 
+      className="background"
+      style={{backgroundColor:randomColor , transition}}>
+      <div id="quote-box">
+        <div className="quote-content" style={{color:randomColor, transition}}>
+          <h2 id="text">
+            <FaQuoteLeft size="30" style={{marginRight:"10px"}}/>
+            {quote.quote}
+            <FaQuoteRight size="30" style ={{marginLeft: "10px"}}/>
+          </h2>
+          <h4 id="author">- {quote.author}</h4>
+        </div>
+        <div className="buttons">
+          <a href="twitter.com/intent/tweet" id="tweet-quote"
+          style={{
+            backgroundColor: randomColor,
+            marginRight: "20px",transition
+          }}>
+            <FaTwitter color="white"/>
+          </a>
+          <button id="new-quote" onClick={changeQuote} style={{backgroundColor: randomColor}}>Change Quote</button>
+        </div>
+      </div>
+
+    </div>
+  );
+  
+}
+
+export default App;
